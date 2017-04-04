13a2cbbb0844e53d58e3a1d2274ec6006c648f71
EKillick <e.killick@uea.ac.uk>
2017-04-04 01:23:02 +0100

    Initial git implementation

---
+++ script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#A script to ping servers and save the results
+#A folder is created for each host and the script iterates through these
+#The folder names must be the same as the addresses being pinged
+for d in * ; do
+    if [[ -d $d ]]; then                            #Only runs on folder names
+        date >> $d/full.log                         #Appends the current datetime to the logfiles
+        date >> $d/summary.log
+        ping -c 300 $d >> $d/full.log               #Appends ping results to one file
+        tail -3 $d/full.log >> $d/summary.log       #Adds summary lines to a separate file
+        echo $'\n\n' >> $d/full.log                 #Adds new lines to each file
+        echo $'\n' >> $d/summary.log
+    fi
+done
