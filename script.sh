99c8e64b420a8d6c1c86c830c30079e03266906a
EKillick <e.killick@uea.ac.uk>
2017-04-04 02:52:23 +0100

    Updated script

--- script.sh
+++ script.sh
@@ -4,9 +4,10 @@
 #The folder names must be the same as the addresses being pinged
 for d in * ; do
     if [[ -d $d ]]; then                            #Only runs on folder names
+        echo "$d"
         date >> $d/full.log                         #Appends the current datetime to the logfiles
         date >> $d/summary.log
-        ping -c 300 $d >> $d/full.log               #Appends ping results to one file
+        ping -c 100 $d >> $d/full.log               #Appends ping results to one file
         tail -3 $d/full.log >> $d/summary.log       #Adds summary lines to a separate file
         echo $'\n\n' >> $d/full.log                 #Adds new lines to each file
         echo $'\n' >> $d/summary.log
